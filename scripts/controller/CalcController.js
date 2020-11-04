118354c7e0054c4b7ba2cb0f4951c52d7bcd8de6
evenilson <evenilsonlp@gmail.com>
2020-11-03 22:35:14 -0300

    oo e mvc + método construtor e encapsulamento

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,24 @@
+class CalcController{
+
+    constructor(){
+        this._displayCalc = "0";
+        this._dataAtual;
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(value){
+        this._dataAtual = value;
+    }
+
+}
