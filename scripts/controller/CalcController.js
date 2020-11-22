7bd375114f88343b1b31180f767af6e8d494aff6
evenilson <evenilsonlp@gmail.com>
2020-11-21 22:20:26 -0300

    validando o primero operador e calculando o result

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -47,32 +47,64 @@ class CalcController {
         return (['+', '-', '*','+', '%', '/'].indexOf(value) > -1);
     }
 
+    pushOperation(value){
+        this._operation.push(value);
+
+        if(this._operation.length > 3){
+
+            this.calc();
+        }
+    }
+    
+    calc(){
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
+    }
+
+    setLastNumberToDisplay(){
+        let lastNumber;
+
+        for(let i = this._operation.length -1; i >= 0; i--){
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
+    }
+
     addOperation(value) {
 
-        console.log("A", isNaN(this.getLastOperation()));
 
         if(isNaN(this.getLastOperation())){
             if(this.isOperator(value)){
                 this.setLastOperation(value);
             }else if(isNaN(value)){
-                console.log(value);
+                console.log('outra coisa', value);
             }else{
-                this._operation.push(value)
+                this.pushOperation(value)
+
+                this.setLastNumberToDisplay();
             }
         }
         else{
             if(this.isOperator(value)){
-                this._operation.push(value)
+                this.pushOperation(value)
             }else{
                 let newValue = this.getLastOperation().toString() + value.toString();
                 this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
             }
             
         }
 
-        
-
-        console.log(this._operation);
     }
 
     setError() {
