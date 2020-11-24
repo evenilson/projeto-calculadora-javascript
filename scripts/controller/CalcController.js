16b4be67ae601431046f2ff73ad615c5449c653d
evenilson <evenilsonlp@gmail.com>
2020-11-23 22:28:53 -0300

    com bug de clicar 2 iguais

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,9 @@
 class CalcController {
     constructor() {
+
+        this._lastOperator ='';
+        this._lastNumber = '';
+
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -61,17 +65,34 @@ class CalcController {
             this.calc();
         }
     }
+
+    getResult(){
+        return eval(this._operation.join(""));
+    }
     
     calc(){
 
-
         let last ='';
+
+        this._lastOperator = this.getLastItem();
+
+
+        if(this._operation.length < 3){
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this.lastNumber];
+        }
 
         if(this._operation.length > 3){
             last = this._operation.pop();
+
+            this._lastNumber = this.getResult();
+
+        } else if(this._operation.length == 3){
+
+            this._lastNumber = this.getLastItem(false);
         }
 
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
 
         if(last == '%'){
 
@@ -85,20 +106,33 @@ class CalcController {
             if(last) this._operation.push(last);
         }
 
-      
-
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay(){
-        let lastNumber;
+
+    getLastItem(isOperator = true){
+        let lastItem; 
 
         for(let i = this._operation.length -1; i >= 0; i--){
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
-                break;
-            }
+
+            if(isOperator){
+                if(this.isOperator(this._operation[i]) == isOperator){
+                    lastItem = this._operation[i];
+                    break;
+                }
+            } 
         }
+
+        if(!lastItem){
+            lastItem = (isOperator) ? this._lastOperator : this.lastNumber;
+        }
+
+        return lastItem;
+    }
+
+    setLastNumberToDisplay(){
+        
+        let lastNumber = this.getLastItem(false);
 
         if(!lastNumber) lastNumber = 0;
 
